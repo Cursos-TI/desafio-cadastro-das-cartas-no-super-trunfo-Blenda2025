87c326b0ebdce4ff6ac590f6ff91f5dfcd9b7598
Blenda2025 <blendarodrigues@hotmail.com>
2025-02-20 01:40:48 +0000

    Finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,6 +8,13 @@ int main(){
   float Area = 218.8;
   float PIB = 33.094;
   int Turisticos = 30;
+  char Estado2[50] = "Paraiba";
+  char Codigo2[50] = "B01";
+  char Cidade2[50] = "Patos"; 
+  int Populacao2 = 113000;
+  float Area2 = 513;
+  float PIB2 = 18.329;
+  int Turisticos2 = 10;
 
   printf("Estado: %s\n", Estado);
   printf("Codigo da Carta: %s\n",Codigo);
@@ -16,5 +23,13 @@ int main(){
   printf("Area: %.3f\n", Area);
   printf("PIB: %.3f\n", PIB);
   printf("Pontos Turisticos: %d\n", Turisticos);
+  
+  printf("Estado: %s\n", Estado2);
+  printf("Codigo da Carta: %s\n",Codigo2);
+  printf("Cidade: %s\n", Cidade2);
+  printf("Populacao: %d\n", Populacao2);
+  printf("Area: %.3f\n", Area2);
+  printf("PIB: %.3f\n", PIB2);
+  printf("Pontos Turisticos: %d\n", Turisticos2);
 
 }
